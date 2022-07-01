cbfe4ffd0d05b650d912e3646842dae5de00d05d
atetheone <atevirran@gmail.com>
2022-06-30 18:03:32 -0700

    Update again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,7 @@
 
+#!/bin/bash
+
+int main(void) { 
+	printf("Programming is like building a multilangual puzzle");	
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+/**
+ *  main: Entry point
+ *
+ *  Description: prints with proper grammar
+ *
+ *  Return Always 0 (Success)
+ */
+
+int main(void) {
+	printf("With proper grammar, but the commit is a piece of art, \n");
+	return (0);
+}
