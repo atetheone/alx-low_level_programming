014f541c6831f336e0c18fce5fd5e64a5d1143ce
atetheone <atevirran@gmail.com>
2022-07-04 11:23:53 -0700

    betty style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,7 @@
 #include <time.h>
 /*
  * main - check the sign of a random number generated
- *	it prints a sentence for the number sign
- * 
+ * it prints a sentence for the number sign
  * Return: Always 0 for success
  *
  */
@@ -14,13 +13,11 @@ int main(void)
 	int n;
 	srand(time(NULL));
 	n = rand() - RAND_MAX / 2;
-
 	if (n < 0)
 		printf("%d is negative\n", n);
 	else if (n > 0)
 		printf("%d is positive\n", n);
 	else
 		printf("%d is zero\n", n);
-
 	return (0);
 }
