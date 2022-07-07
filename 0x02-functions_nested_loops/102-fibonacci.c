d7dabc906d978853f4177fa7150f2d5a4d6442fe
atetheone <atevirran@gmail.com>
2022-07-06 20:09:20 -0700

    fibnacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - fibonacci numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n, a, b, fib, i;
+	
+	a = 0;
+	b = 1;
+	printf("%d, %d, ", a, b);
+	for (i = 2; i <= 50; i++)
+	{
+		fib = a + b;
+		a = b;
+		b = fib;
+		if ( i < 50)
+			printf("%d, ", fib);
+		else
+			printf("%d", fib);
+	}
+	return (0);
+}
