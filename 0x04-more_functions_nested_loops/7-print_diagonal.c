df7d21d9bf1f0aea16f2591ce6de220483b49d40
atetheone <atevirran@gmail.com>
2022-07-08 19:16:14 -0700

    betty style fixing

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,25 +1,26 @@
 #include "main.h"
 
 /**
- * print_diagonal - a function that draws a diagonal line on the terminal.
- * @n: An input integer
- * Return: Always 0
+ * print_diagonal - prints a diagonal.
+ * @n: diagonal length
+ * Return: no return.
  */
 void print_diagonal(int n)
 {
-	int i , j;
+	int i, j;
 
-        
-	if (n > 0)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
 		{
-			for (j = 0; j < i; j++)
+			if (i >= 2 && j <= i - 1)
 				_putchar(' ');
-		        _putchar(92);
-		        _putchar('\n');
+			else
+				_putchar('\\');
 		}
-	}
-	else
+		if (i == n - 1)
+			break;
 		_putchar('\n');
+	}
+	_putchar('\n');
 }
