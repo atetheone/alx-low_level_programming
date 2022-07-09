0ed87d31bd8799cf9c9853eb3bfb743610e56234
atetheone <atevirran@gmail.com>
2022-07-08 19:56:24 -0700

    betty style fixing

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,11 +13,11 @@ void print_triangle(int size)
 	if (n <= 0)
 		_putchar('\n');
 
-	for (i = 0; i < n; i++)
+	for (i = 1; i <= n; i++)
 	{
 		for (j = i; j < n; j++)
 			_putchar(' ');
-		for (j = 0; j < i; j++)
+		for (j = 1; j <= i; j++)
 			_putchar('#');
 		_putchar('\n');
 	}
