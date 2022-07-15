7ccec817d5200558496cb04ee06c54eb4bb97372
atetheone <atevirran@gmail.com>
2022-07-15 07:33:36 -0700

    1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,19 +7,10 @@
  * @src: second string, the source
  * @n: number of letters to concatenate from the second string
  * Return: dest = dest + src(from 0 to n)
-=======
-/**
- * _strncat - concatenates two strings,
- * @dest: destination.
- * @src: source.
- * @n: amount of bytes used from src.
- * Return: the pointer to dest.
->>>>>>> 05c80e363570862372a29536a59bd12a7495722f
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-<<<<<<< HEAD
 	int len = 0;
 	int len2 = 0;
 
@@ -34,22 +25,5 @@ char *_strncat(char *dest, char *src, int n)
 		len2++;
 	}
 	*(dest + len) = '\0';
-=======
-	int count = 0, count2 = 0;
-
-	while (*(dest + count) != '\0')
-	{
-		count++;
-	}
-
-	while (count2 < n)
-	{
-		*(dest + count) = *(src + count2);
-		if (*(src + count2) == '\0')
-			break;
-		count++;
-		count2++;
-	}
->>>>>>> 05c80e363570862372a29536a59bd12a7495722f
 	return (dest);
 }
