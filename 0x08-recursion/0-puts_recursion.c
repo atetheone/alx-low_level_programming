e79dc67b713613935a64b1fe096833c93e5fd6cd
atetheone <atevirran@gmail.com>
2022-07-20 05:01:58 -0700

    puts recursion version

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _puts_recursion - puts recursion
@@ -11,9 +10,9 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		printf("\n");
+		_putchar('\n');
 		return;
 	}
-	printf("%c", *s);
+	_putchar(*s);
 	_puts_recursion(s + 1);
 }
