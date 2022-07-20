42d06a668f647a48d5fbcadc7e2f8e0833d9cb8c
atetheone <atevirran@gmail.com>
2022-07-20 05:39:08 -0700

    pow(x, y) rec

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,11 +12,11 @@ int _pow_recursion(int x, int y)
 	if (y < 0)
 		return (-1);
 	if (y == 0)
-		return (0);
+		return (1);
 	if (y == 1)
 		return (x);
 	if (y % 2 == 0)
-		return _pow_recursion(x, y / 2) * _pow_recursion(x, y / 2);
+		return (_pow_recursion(x, y / 2) * _pow_recursion(x, y / 2));
 	else
-		return x * _pow_recursion(x, (y - 1) / 2) * _pow_recursion(x, (y - 1) / 2);
+		return (x * _pow_recursion(x, (y - 1) / 2) * _pow_recursion(x, (y - 1) / 2));
 }
