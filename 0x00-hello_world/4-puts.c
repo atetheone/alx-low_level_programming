8590f44512166622b9ea3f0ad83f2badae161e3b
atetheone <atevirran@gmail.com>
2022-09-07 23:11:31 -0700

    Hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print a sentence using puts only
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");	
+return (0);
+}
